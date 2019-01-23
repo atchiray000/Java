b269eaeefde2f7ce68b2c02319bfb2fd1d86bc8b
atchiray000 <45366023+atchiray000@users.noreply.github.com>
2019-01-22 18:36:09 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,50 +1,16 @@
- function buildQuiz(){}
+(function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
 
-function showResults(){}
-buildQuiz();
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-  {
-    question: "How to make text bold with HTML?",
-    answers: {
-      a: "<i>",
-      b: "<br>",
-      c: "<b>"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "0011?",
-    answers: {
-      a: "3",
-      b: "2",
-      c: "79"
-    },
-    correctAnswer: "b"
-  },
-  {
-    question: "2+2?",
-    answers: {
-      a: "22",
-      b: "4",
-      c: "None of the Above",
-    },
-    correctAnswer: "b"
-  }
-];
-
-
-function buildQuiz(){
-  const output = [];
-
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
       const answers = [];
 
-      for(letter in currentQuestion.answers){
-
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
         answers.push(
           `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -54,114 +20,86 @@ function buildQuiz(){
         );
       }
 
+      // add this question and its answers to the output
       output.push(
         `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+        <div class="answers"> ${answers.join("")} </div>`
       );
-    }
-  );
+    });
 
-  quizContainer.innerHTML = output.join('');
-}
-
-myQuestions.forEach( (currentQuestion, questionNumber) => {
-});
-
-
-const answers = [];
-
-for(letter in currentQuestion.answers){
-
-  answers.push(
-    `<label>
-      <input type="radio" name="question${questionNumber}" value="${letter}">
-      ${letter} :
-      ${currentQuestion.answers[letter]}
-    </label>`
-  );
-}
-
-output.push(
-  `<div class="question"> ${currentQuestion.question} </div>
-  <div class="answers"> ${answers.join('')} </div>`
-);
-
-quizContainer.innerHTML = output.join('');
-
-function showResults(){
-
-
-  const answerContainers = quizContainer.querySelectorAll('.answers');
-
-  let numCorrect = 0;
-
-
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-    const answerContainer = answerContainers[questionNumber];
-    const selector = 'input[name=question'+questionNumber+']:checked';
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-
-    if(userAnswer===currentQuestion.correctAnswer){
-
-      numCorrect++;
-
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-
-    else{
-      answerContainers[questionNumber].style.color = 'red';
-    }
-  });
-  resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
-}
-
-const answerContainers = quizContainer.querySelectorAll('.answers'); 
-
-let numCorrect = 0;
-
-
-myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-
-  const answerContainer = answerContainers[questionNumber];
-  const selector = `input[name=question${questionNumber}]:checked`;
-  const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-
-  if(userAnswer===currentQuestion.correctAnswer){
-
-    numCorrect++;
-
-
-    answerContainers[questionNumber].style.color = 'lightgreen';
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
   }
 
-  else{
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    answerContainers[questionNumber].style.color = 'red';
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
-});
 
-const answerContainer = answerContainers[questionNumber];
-const selector = `input[name=question${questionNumber}]:checked`;
-const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
+    }
+  ];
 
+  // display quiz right away
+  buildQuiz();
 
-if(userAnswer===currentQuestion.correctAnswer){
-
-  numCorrect++;
-
-
-  answerContainers[questionNumber].style.color = 'lightgreen';
-}
-// if answer is wrong or blank
-else{
-
-  answerContainers[questionNumber].style.color = 'red';
-}
-
-resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-(function(){
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
 })();
