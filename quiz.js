2e6c27e61c89b8c6191412b4bebf6815b19f7c97
atchiray000 <45366023+atchiray000@users.noreply.github.com>
2019-01-22 18:44:36 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
 (function() {
   function buildQuiz() {
-    // we'll need a place to store the HTML output
+
     const output = [];
 
-    // for each question...
+
     myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
+
       const answers = [];
 
-      // and for each available answer...
+
       for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
+
         answers.push(
           `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -20,46 +20,43 @@
         );
       }
 
-      // add this question and its answers to the output
       output.push(
         `<div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join("")} </div>`
       );
     });
 
-    // finally combine our output list into one string of HTML and put it on the page
+
     quizContainer.innerHTML = output.join("");
   }
 
   function showResults() {
-    // gather answer containers from our quiz
+
     const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
+
     let numCorrect = 0;
 
-    // for each question...
+
     myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
+
       const answerContainer = answerContainers[questionNumber];
       const selector = `input[name=question${questionNumber}]:checked`;
       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // if answer is correct
       if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
+
         numCorrect++;
 
-        // color the answers green
+
         answerContainers[questionNumber].style.color = "lightgreen";
       } else {
-        // if answer is wrong or blank
-        // color the answers red
+
         answerContainers[questionNumber].style.color = "red";
       }
     });
 
-    // show number of correct answers out of total
+
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
 
@@ -68,38 +65,36 @@
   const submitButton = document.getElementById("submit");
   const myQuestions = [
     {
-      question: "Who is the strongest?",
+      question: "How do you make text bold with HTML?",
       answers: {
-        a: "Superman",
-        b: "The Terminator",
-        c: "Waluigi, obviously"
+        a: "<i>",
+        b: "<br>",
+        c: "<b>"
       },
       correctAnswer: "c"
     },
     {
-      question: "What is the best site ever created?",
+      question: "What room are we currently in?",
       answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
+        a: "SE 224",
+        b: "NW 209",
+        c: "SE 226"
       },
-      correctAnswer: "c"
+      correctAnswer: "a"
     },
     {
-      question: "Where is Waldo really?",
+      question: "2+2?",
       answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
+        a: "22",
+        b: "4",
       },
-      correctAnswer: "d"
+      correctAnswer: "b"
     }
   ];
 
-  // display quiz right away
+
   buildQuiz();
 
-  // on submit, show results
+
   submitButton.addEventListener("click", showResults);
 })();
